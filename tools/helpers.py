58c1eea01e412fe4f819e67c6a1fcae353252e89
Daniel Suess <daniel.suess@web.de>
2014-11-06 14:42:55 +0100

    Implement getch function

--- tools/helpers.py
+++ tools/helpers.py
@@ -3,8 +3,13 @@
 """Little helpers for everyday python workLittle helpers for everyday python work.."""
 
 from __future__ import division
+
+import os
 import sys
 from time import time
+
+import fcntl
+import termios
 
 
 class Timer(object):
@@ -85,3 +90,26 @@ class Progress(object):
             print ""
 
         return self._iterable.next()
+
+
+def getch():
+    """Gets a single character from the console."""
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    try:
+        while 1:
+            try:
+                c = sys.stdin.read(1)
+                return c
+            except IOError: pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
